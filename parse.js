9ccce62e2839e0e04751ead25528fcee8010ef01
Kellman <kellman@meghu.ca>
2019-01-25 06:58:40 -0500

    new parsing test:

--- parse.js
+++ parse.js
@@ -4,30 +4,64 @@ const pagein = require('./fun/page')
 const grabin = require('./fun/grab')
 //
 var dl = require('datalib');
-const rebuild = require('./last.json')
+var data = dl.json('./last.json')
+//const rebuild = require('./last.json')
 const Cpclass = require('./class/cpobj')
 const CpApi = require('./class/cpapi')
 const setKey = require('./fun/writekey')
 const netroot = 'net/'
 
-async function runtime () {
+runtime(data)
+.then(parsein)
+.then(JSON.stringify)
+.then(console.log)
+
+async function runtime (x) {
 	try {
-		var myobj = await dl.isObject(rebuild)
-		var myarr = await dl.isArray(rebuild)
-		console.log(await myobj)
-		console.log(await myarr)
-		var mykeys = await dl.keys(rebuild)
-		//var mytree = await dl.treejson(rebuild, mykeys)
-		console.log(await mykeys)
-		//console.log(await mytree)
+		//var myobj = dl.isObject(x)
+		//var myarr = dl.isArray(x)
+		//console.log(x + ' is object: ' + myobj)
+		//console.log(x + ' is array: ' + myarr)
+		var mykeys = dl.keys(x)
+		return mykeys
 	} catch (err) {
 		console.log(err.message)
-		console.log('PROGRAM ERROR')
+		//console.log('PROGRAM ERROR')
 	} finally {
-		console.log('PROGRAM EXIT')
+		//console.log('PROGRAM EXIT')
 	}
 }
 
+async function parsein (x) {
+	try {
+		var myobjarr = {}
+		for (var keys in x) {
+			//console.log(x[keys])
+			let kname = x[keys]
+			myobjarr[kname] = []
+			myobjarr[kname].push(await dl.vals(data[x[keys]]))
+		}
+		return await myobjarr
+	} catch (err) {
+		//console.log('PARSEIN ERROR')
+		return
+	}
+}
 
-runtime()
-
+async function getvals (x) {
+	try {
+		//console.log(x)
+		var myvals = []
+		for (var objs in x) {
+			myvals.push(dl.vals(x[objs]))
+			//myvals.objects = dl.vals(x[objs])
+		}
+		return myvals
+	} catch (err) {
+		return
+		//console.log(err.message)
+		//console.log('GETVALS ERROR')
+	} finally {
+		//console.log('GETVALS EXIT')
+	}
+}
