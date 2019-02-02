c1858fb49ec26d9f33acbcff733f757b927cb743
Kellman <kellman@meghu.ca>
2019-02-02 12:17:13 -0500

    remove logging info

--- fun/cpapi.js
+++ fun/cpapi.js
@@ -2,7 +2,6 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const axios = require('axios')
 
 module.exports = function (x) {
-	//console.dir(x)
 	return new Promise(function(resolve, reject) {
 		axios.defaults.headers.common['Accept'] = 'application/json'
 		axios(x)
