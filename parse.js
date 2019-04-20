271c3eef7aa970b83e35b6237fdeff101fb450a7
kellmant <kellman@meghu.ca>
2019-04-20 08:12:15 -0400

    updated for new object testing

--- parse.js
+++ parse.js
@@ -4,25 +4,38 @@ const pagein = require('./fun/page')
 const grabin = require('./fun/grab')
 //
 var dl = require('datalib');
-var data = dl.json('./last.json')
+var data = require('./mychange.json')
+var groupBy = require('json-groupby')
+var diff = require('variable-diff')
+//const ddiff = require("deep-object-diff").diff
+const changesets = require("diff-json")
 //const rebuild = require('./last.json')
 const Cpclass = require('./class/cpobj')
 const CpApi = require('./class/cpapi')
 const setKey = require('./fun/writekey')
+const dump = require('./fun/writefile')
 const netroot = 'net/'
+var mykeys = []
+const mychanges = {}
 
 runtime(data)
 .then(parsein)
-.then(JSON.stringify)
-.then(console.log)
+.then(getvals)
+//.then(console.dir)
 
 async function runtime (x) {
 	try {
-		//var myobj = dl.isObject(x)
-		//var myarr = dl.isArray(x)
-		//console.log(x + ' is object: ' + myobj)
-		//console.log(x + ' is array: ' + myarr)
-		var mykeys = dl.keys(x)
+		var toparr = dl.isArray(x)
+		//console.log(' var ' + x + ' is array: ' + toparr + ' \n')
+		for (var o in x) {
+			var myobj = dl.isObject(x[o])
+			var myarr = dl.isArray(x[o])
+			//console.log(' key ' + o + ' is object: ' + myobj)
+			//console.log(' key ' + o + ' is array: ' + myarr + ' \n')
+			//console.log(x[o])
+			mykeys = mykeys.concat(x[o])
+		}
+		//await dump('pdmp', mykeys)
 		return mykeys
 	} catch (err) {
 		console.log(err.message)
@@ -34,13 +47,17 @@ async function runtime (x) {
 
 async function parsein (x) {
 	try {
-		var myobjarr = {}
-		for (var keys in x) {
-			//console.log(x[keys])
-			let kname = x[keys]
-			myobjarr[kname] = []
-			myobjarr[kname].push(await dl.vals(data[x[keys]]))
+		var myobjarr = []
+		//myobjarr = groupBy(x, ['uid'])
+		//let kname = x[keys].type
+		for (var ops in x) {
+			//myobjarr = x.concat(dl.vals(x[keys]))
+			//console.log(x[ops].operations['modified-objects'])
+			myobjarr = myobjarr.concat(x[ops].operations['modified-objects'])
 		}
+		console.log('\n')
+		//myobjarr = groupBy(myobjarr, ['uid'])
+		//await dump('prr', myobjarr)
 		return await myobjarr
 	} catch (err) {
 		//console.log('PARSEIN ERROR')
@@ -52,11 +69,27 @@ async function getvals (x) {
 	try {
 		//console.log(x)
 		var myvals = []
-		for (var objs in x) {
-			myvals.push(dl.vals(x[objs]))
-			//myvals.objects = dl.vals(x[objs])
+		var mydiffs = []
+		for (var obj in x) {
+			//myvals.push(x[obj])
+			//myvals[obj] = []
+			let newobj = x[obj]['new-object']
+			let oldobj = x[obj]['old-object']
+			var nameobj = newobj['name']
+			mychanges[nameobj] = []
+			let nameit = 'ddd_' + obj
+			//await dump(nameit + 'new', newobj)
+			//await dump(nameit + 'old', oldobj)
+			//myvals = myvals.concat(ddiff(oldobj, newobj))
+			delete oldobj['meta-info']
+			delete newobj['meta-info']
+			mychanges[nameobj] = mychanges[nameobj].concat(changesets.diff(oldobj, newobj))
+			let shdiff = diff(oldobj, newobj)
+			console.log(nameobj)
+			console.log(shdiff.text + ' \n')
 		}
-		return myvals
+		await dump('ddd_', mychanges)
+		return await mychanges
 	} catch (err) {
 		return
 		//console.log(err.message)
@@ -65,3 +98,9 @@ async function getvals (x) {
 		//console.log('GETVALS EXIT')
 	}
 }
+
+async function dumpout (x) {
+	await dump('out', x)
+	return x
+}
+
